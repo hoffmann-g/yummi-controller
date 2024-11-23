82fe00bbec3283fd3394ee51d8dae777f47c80b6
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-11-23 17:27:54 -0300

    feat: add server

--- src/server.py
+++ src/server.py
@@ -1,13 +1,19 @@
 import os
 import socket
+import threading
+import time
 
-from pynput.mouse import Controller
+from pynput.keyboard import Controller as KeyboardController
+from pynput.mouse import Controller as MouseController
 from dotenv import load_dotenv
+from screeninfo import get_monitors
 from termcolor import colored
+
 
 load_dotenv()
 
-mouse = Controller()
+mouse = MouseController()
+keyboard = KeyboardController()
 
 host = os.getenv("SERVER_HOST")
 port = int(os.getenv("SERVER_PORT"))
@@ -17,6 +23,52 @@ server_socket.bind((host, port))
 server_socket.listen(1)
 
 print(colored(f"# YUMMI CONTROLLER server started on port {port}", "green"))
+
+screen_width = get_monitors()[0].width
+screen_height = get_monitors()[0].height
+
+key_press_duration = float(os.getenv("KEY_PRESS_DURATION"))
+
+def move_mouse_to_coords(x, y):
+    screen_x = int(x * screen_width)
+    screen_y = int(y * screen_height)
+
+    mouse.position = (screen_x, screen_y)
+
+def press_key(key: str):
+    try:
+        keyboard.press(key)
+
+        time.sleep(key_press_duration)
+
+        keyboard.release(key)
+
+    except ValueError as ex:
+        print(colored(f"Invalid key: {key}. Error: {ex}", "red"))
+
+    except Exception as ex:
+        print(colored(f"Unexpected error while pressing {key}: {ex}", "red"))
+
+def handle_input(command : str):
+    try:
+        if command.startswith("mouse_coords"):
+            parts = command.split(":")[1].strip()
+            x, y = map(float, parts.split(","))
+
+            threading.Thread(target=move_mouse_to_coords, args=(x, y), daemon=True).start()
+            print(colored("- moving mouse...", "light_green"))
+
+            return
+
+        if command.startswith("key"):
+            key = command.split(":")[1].strip()
+
+            threading.Thread(target=press_key, args=(key,), daemon=True).start()
+            print(colored(f"- pressed key [{key}]", "light_green"))
+
+            return
+    except ValueError as ex:
+        return
 
 while True:
     try:
@@ -31,7 +83,9 @@ while True:
                 print(colored(f"# Client {addr} disconnected.", "yellow"))
                 break
 
-            print(data)
+            commands = data.split(";")
+            for comm in commands:
+                handle_input(comm)
 
         conn.close()
 
